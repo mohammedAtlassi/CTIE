cd90199ec55ad401d396d88a21d99176267cbf3e
Mohammed Atlassi <mohammed.atlassi@gmail.com>
2021-06-03 11:52:12 +0200

    "no-tabs": 0,

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'linebreak-style': ['error', 'windows'],
     'max-len': ['error', { code: 1200 }],
     "linebreak-style": 0,
+    "no-tabs": 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
