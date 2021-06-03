05bfe53ce357f29ad91ba2eaf4a138d2cb6c8b6c
Mohammed Atlassi <mohammed.atlassi@gmail.com>
2021-06-03 10:11:00 +0200

    update FL

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   rules: {
     'linebreak-style': ['error', 'windows'],
     'max-len': ['error', { code: 1200 }],
+    "linebreak-style": 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
