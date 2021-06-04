31029018956da2ca6fbf88970282052bc03a8bc7
mohammedAtlassi <mohammed.atlassi@gmail.com>
2021-06-04 15:57:18 +0200

    correct server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 app = express();
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 app.get(/.*/, function(req, res){
-  res.sendFile(path.join(__dirname, , '/dist/index.html'));
-}
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+})
 const port = process.env.PORT || 8080;
 app.listen(port);
 console.log('app is listenning on port: ${port}');
